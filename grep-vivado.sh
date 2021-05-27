7b97bd42f525eb36e1998853b768364a3a44e1ef
Peter Lebbing <peter@digitalbrains.com>
2021-05-27 18:07:29 +0200

    fixup! Run test benches in Vivado

---
+++ grep-vivado.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mkdir -p "$(dirname $0)"/build
+cd "$(dirname $0)"/build || exit $?
+
+vivado -mode tcl -source ../vivado/simulate.tcl -tclargs "$@" | \
+  grep -E '^Wall time|^Testbench|^Error|^WARNING'
+rm -rf xilinxfloat
