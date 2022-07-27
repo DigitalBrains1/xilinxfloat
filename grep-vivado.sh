44a8f0b43f0b344227bcc35312a586fe97fb4a81
Peter Lebbing <peter@digitalbrains.com>
2022-07-27 16:24:03 +0200

    Report critical warnings

--- grep-vivado.sh
+++ grep-vivado.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 tail -n+0 -F "$(dirname $0)/build/vivado.log" | \
-  grep -E '^Wall time|^Testbench|^Error|^WARNING|^@'
+  grep -E '^Wall time|^Testbench|^Error|^ERROR|^CRITICAL WARNING|^WARNING|^@'
